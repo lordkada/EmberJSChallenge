83cf99d0b193c43a2ad4a90b34ac456f4eae72d8
Vivek Munde <vivekmundegmail.com@MUNDS-MAC.local>
2016-06-02 18:57:36 +0530

    image upload

--- app/components/appliances/appliance-new.js
+++ app/components/appliances/appliance-new.js
@@ -3,12 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     appliances: Ember.inject.service('appliances-list'),
     actions: {
-        next() {
-            this.sendAction('next');
+        submit(appliance) {
+            this.get('appliances').addNewItem(appliance);
+            this.get('appliances').clearNewItem();
+            alert('The appliance \'' + appliance.model + '\' is added successfully!');
+            this.sendAction('submit');
         },
         cancel() {
             this.get('appliances').clearNewItem();
             this.sendAction('cancel');
+        },
+        fileSelectionChanged(file) {
+            this.get('appliances').setNewItemFile(file);
         }
     }
 });
--- app/routes/appliances/new.js
+++ app/routes/appliances/new.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     actions: {
-        next() {
-            this.transitionTo('appliances.new-images');
+        submit() {
+            this.transitionTo('appliances');
         },
         cancel() {
             this.transitionTo('appliances');
